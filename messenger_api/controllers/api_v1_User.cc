3a43916c6ae95434beab942d315a05d8e8bf638d
joegasewicz <joegasewicz@gmail.com>
2024-05-19 23:15:25 +0100

    handlers

--- messenger_api/controllers/api_v1_User.cc
+++ messenger_api/controllers/api_v1_User.cc
@@ -3,3 +3,14 @@
 using namespace api::v1;
 
 // Add definition of your processing function here
+
+
+
+void User::login(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
+    LOG_DEBUG << "GET /users";
+    Json::Value ret;
+    ret["id"] = "1";
+    ret["token"] = "<TOKEN>";
+    auto resp = HttpResponse::newHttpJsonResponse(ret);
+    callback(resp);
+}
--- messenger_api/controllers/api_v1_User.h
+++ messenger_api/controllers/api_v1_User.h
@@ -16,11 +16,13 @@ class User : public drogon::HttpController<User>
     // METHOD_ADD(User::get, "/{2}/{1}", Get); // path is /api/v1/User/{arg2}/{arg1}
     // METHOD_ADD(User::your_method_name, "/{1}/{2}/list", Get); // path is /api/v1/User/{arg1}/{arg2}/list
     // ADD_METHOD_TO(User::your_method_name, "/absolute/path/{1}/{2}/list", Get); // path is /absolute/path/{arg1}/{arg2}/list
-
+    METHOD_ADD(User::login, "/users", Get);
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, int p1, std::string p2);
     // void your_method_name(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, double p1, int p2) const;
+    void login(const HttpRequestPtr &req,
+               std::function<void (const HttpResponsePtr &)> &&callback);
 };
 }
 }
