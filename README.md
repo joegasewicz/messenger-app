f4e9e377438ef880cd841526fb0088291745ca2e
joegasewicz <joegasewicz@gmail.com>
2024-05-19 22:03:05 +0100

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Messenger App
+A Mobile Messenger POC app.
+
+
+### Tech-stack
+- Drogon / C++
+- Flutter / Dart
+
---
+++ messenger_mobile/android/app/src/main/kotlin/com/messengerapp/messenger_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.messengerapp.messenger_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
